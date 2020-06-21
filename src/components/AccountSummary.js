88249ebf82538658277dfcd6a297b81672e19fc3
Kashan Ahmed <kashan2a@gmail.com>
2020-06-21 23:27:41 +0500

    Account Summary UI Updated

--- src/components/AccountSummary.js
+++ src/components/AccountSummary.js
@@ -5,10 +5,16 @@ export const AccountSummary = () => {
         <div className = "inc-exp-container">
             <div>
             
-                <h4>Income</h4>    
+                <h4>Income</h4> 
+                <p className = "money plus">
+                    $0.00
+                </p>   
             </div>
             <div>
                 <h4>Expense</h4>
+                <p className = 'money minus'>
+                    $0.00
+                </p>
             
             </div>
         </div>
