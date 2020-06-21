6dd7842b4cf0c01d2846621b8191b3f981338cbb
Kashan Ahmed <kashan2a@gmail.com>
2020-06-21 23:22:08 +0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-
 import { Header } from './components/Header'
+import { Balance } from './components/Balance'
+import { AccountSummary} from './components/AccountSummary'
 
 function App() {
   return (
     <div>
       <Header/>
-      
+      <div className="container">
+        <Balance/>
+        <AccountSummary/>
+      </div>
     </div>
   );
 }
