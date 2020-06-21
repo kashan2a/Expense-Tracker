83378af59a3e454a0746254892216c4ab5c33154
Kashan Ahmed <kashan2a@gmail.com>
2020-06-22 00:13:10 +0500

    AddTransaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -4,6 +4,22 @@ export const AddTransaction = () => {
     return (
         <div>
             <h3>Add New Transaction</h3>
+            <form>
+                <div className = "form-control">
+                    <label html For = "description">
+                        Description
+                    </label>
+                    <input type = "text" id = "description" placeholder = "Transactions"></input>
+                </div>
+                <div className = "form-control">
+                    <label htmlFor= "TransactionAmount">
+                        Transaction Ammount
+                    </label>
+                    <input type = "number"
+                           id = "TransactionAmount"
+                           placeholder = 
+                </div>
+            </form>
         </div>
     )
 }
